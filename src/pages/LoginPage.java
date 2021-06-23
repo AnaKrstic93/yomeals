21e5ce5a62e5d9ece4f4f65fa8493e15de5bbae1
Ana Krstic <anakrsticqa@gmail.com>
2021-06-23 17:06:22 +0200

    Getters for LoginPage

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasicPage {
+
+	public LoginPage(WebDriver driver, JavascriptExecutor js) {
+		super(driver, js);
+	}
+
+	public WebElement getUsernameInput () {
+		return this.driver.findElement(By.name("username"));
+	}
+	
+	public WebElement getPasswordInput () {
+		return this.driver.findElement(By.name("password"));
+	}
+	
+	public WebElement getLoginButton () {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+	
+}
