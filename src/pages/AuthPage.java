e2873cb12068d0fda77509d20c174441ef13192e
Ana Krstic <anakrsticqa@gmail.com>
2021-06-25 01:01:16 +0200

    AuthPage

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,5 +1,27 @@
 package pages;
 
-public class AuthPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class AuthPage extends BasicPage {
+
+	public AuthPage(WebDriver driver, JavascriptExecutor js, WebDriverWait wait) {
+		super(driver, js, wait);
+	}
+
+	public WebElement getUserDropDownBtn () {
+	return this.driver.findElement(By.xpath("//li[@class=\"filled \"]/a"));
+	}
+	
+	public WebElement getLogoutBtn () {
+		return this.driver.findElement(By.linkText("Logout"));
+	}
+	
+	public void logOut () {
+		this.getUserDropDownBtn().click();
+		this.getLogoutBtn().click();
+	}
 }
