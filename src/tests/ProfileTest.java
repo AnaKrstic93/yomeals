ce6b956474ea252c1307ba941c6a96d3eb2e302f
Ana Krstic <anakrsticqa@gmail.com>
2021-06-25 04:55:04 +0200

    ProfileTest changeProfileImageTest

--- src/tests/ProfileTest.java
+++ src/tests/ProfileTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -22,6 +25,27 @@ public class ProfileTest extends BasicTest {
 								   "England", 
 								   "London");
 		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Setup Successful"));
+		
+		authPage.logOut();
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successful"));
+	}
+	
+	@Test (priority = 2)
+	public void changeProfileImageTest () throws IOException {
+		this.driver.get(this.baseUrl + "/guest-user/login-form");
+		locationPopupPage.closeLocationDialog();
+		loginPage.logIn(this.email, this.password);
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Login Successful"));
+		
+		this.driver.get(this.baseUrl + "/member/profile");
+		String imgPath = new File ("img//image.png").getCanonicalPath();
+		profilePage.uploadPhoto(imgPath);
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Uploaded Successfully"));
+		notificationSystemPage.messageDisappearWait();
+		
+		profilePage.removePhoto();
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Profile Image Deleted Successfully"));
+		notificationSystemPage.messageDisappearWait();
 		
 		authPage.logOut();
 		Assert.assertTrue(notificationSystemPage.getMessageText().contains("Logout Successful"));
