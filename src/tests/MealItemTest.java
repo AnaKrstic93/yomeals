6829bf8c06b9ae9f598c846d967fe8365924d16f
Ana Krstic <anakrsticqa@gmail.com>
2021-06-25 17:08:32 +0200

    MealItemTest clearCartTest

--- src/tests/MealItemTest.java
+++ src/tests/MealItemTest.java
@@ -1,7 +1,14 @@
 package tests;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class MealItemTest extends BasicTest {
 
@@ -43,4 +50,33 @@ public class MealItemTest extends BasicTest {
 			      "[ERROR] Product addition to favourites faliure.");
 	}
 	
+	@Test (priority = 3)
+	public void clearCartTest () throws IOException, InterruptedException {
+		File file = new File ("data/Data.xlsx");
+		FileInputStream fis = new FileInputStream (file);
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("Meals");
+
+		SoftAssert softAssert = new SoftAssert();
+		
+		this.driver.get(this.baseUrl + "/meals");
+		locationPopupPage.setLocationName("City Center - Albany");
+		
+		for (int i = 1; i < sheet.getLastRowNum(); i++) {
+			String mealUrl = sheet.getRow(i).getCell(0).getStringCellValue();
+			int quantity = (int) sheet.getRow(i).getCell(1).getNumericCellValue();
+			
+			Thread.sleep(1000);
+			this.driver.get(mealUrl);
+			mealPage.addMealToCart("\"" + quantity + "\"");
+			
+			softAssert.assertTrue(notificationSystemPage.getMessageText().contains("Meal Added To Cart"),
+				      "[ERROR] Meal addition to cart faliure.");
+		}
+		
+		cartSummaryPage.clearAll();
+		Assert.assertTrue(notificationSystemPage.getMessageText().contains("All meals removed from Cart successfully"),
+			      "[ERROR] Cart clearing faliure.");
+	}
 }
